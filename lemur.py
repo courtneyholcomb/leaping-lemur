c48cb8be68fe56dd400906c19c8115a59a7b1592
Courtney Holcomb <courtneyeholcomb@gmail.com>
2019-09-18 15:32:26 -0700

    solved challenge

--- lemur.py
+++ lemur.py
@@ -30,6 +30,22 @@ def  lemur(branches):
     assert branches[0] == 0, "First branch must be alive"
     assert branches[-1] == 0, "Last branch must be alive"
 
+    jumps = 0
+    safe = {i for i, branch in enumerate(branches) if branch == 0}
+    skip = False
+
+    for num in range(len(branches) - 1):
+        if skip:
+            skip = False
+        elif num + 2 in safe:
+            jumps += 1
+            skip = True
+        else:
+            jumps += 1
+
+    return jumps
+
+
 if __name__ == '__main__':
     import doctest
     if doctest.testmod().failed == 0:
